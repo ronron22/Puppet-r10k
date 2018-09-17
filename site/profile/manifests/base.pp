97b3c89ff21856fcbeea3ac06b55c211e55e8bf8
ronron <ant@n.io>
2018-09-17 12:24:48 +0200

    .

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,3 @@
-class { 'accounts':
-  ssh_keys   => hiera_hash('accounts::ssh_keys', {}),
-  users      => hiera_hash('accounts::users', {}),
-  usergroups => hiera_hash('accounts::usergroups', {}),
+class profile::base {
+    include ntp
 }
